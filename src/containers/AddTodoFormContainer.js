f1a9d253c9e861a2b6fd7b304a8e18d1e0bc7b53
joshuajhun <joshuajhun@gmail.com>
2017-10-16 15:10:09 -0600

    made stylistic changes with how objects are returned

--- src/containers/AddTodoFormContainer.js
+++ src/containers/AddTodoFormContainer.js
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
-import AddTodoForm from '../components/AddTodoForm'
+import AddTodoForm from '../components/AddTodoForm';
 
-const mapStateToProps =  (store) => {
-  return { todos: store.todos }
-}
+const mapStateToProps =  (store) => ({ todos: store.todos
+});
 
 const mapDispatchToProps = (dispatch) => ({
   handleSubmit: (text, id) => {
     dispatch(addTodo(text,id))}
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddTodoForm)
